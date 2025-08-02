0ff0827623985765a60345a3c1a6c39202908af6
Nikita Shyshkin <yatoenough@gmail.com>
2025-08-02 21:33:41 +0200

    Init app starter project

---
+++ SubscriptionsTracker/SubscriptionsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsTrackerApp.swift
+//  SubscriptionsTracker
+//
+//  Created by Nikita Shyshkin on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
