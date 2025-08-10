6faaf738ea006ae395a508966c880676918e72be
Nikita Shyshkin <yatoenough@gmail.com>
2025-08-11 01:47:02 +0200

    Move container initialization to composition root

--- SubscriptionsTracker/SubscriptionsTrackerApp.swift
+++ SubscriptionsTracker/SubscriptionsTrackerApp.swift
@@ -5,24 +5,13 @@
 //  Created by Nikita Shyshkin on 02/08/2025.
 //
 
-import SwiftData
 import SwiftUI
 
 @main
 struct SubscriptionsTrackerApp: App {
-	let modelContainer: ModelContainer = {
-		do {
-			let schema = Schema([Subscription.self])
-			return try ModelContainer(for: schema)
-		} catch {
-			fatalError(error.localizedDescription)
-		}
-	}()
-
 	var body: some Scene {
 		WindowGroup {
 			RootView()
-				.modelContainer(modelContainer)
 		}
 	}
 }
--- SubscriptionsTracker/Views/RootView.swift
+++ SubscriptionsTracker/Views/RootView.swift
@@ -5,16 +5,32 @@
 //  Created by Nikita Shyshkin on 02/08/2025.
 //
 
+import SwiftData
 import SwiftUI
 
 struct RootView: View {
-    var body: some View {
+	let modelContainer: ModelContainer = {
+		do {
+			let schema = Schema([Subscription.self])
+			return try ModelContainer(for: schema)
+		} catch {
+			fatalError(error.localizedDescription)
+		}
+	}()
+
+	var body: some View {
 		NavigationStack {
 			SubscriptionsListView()
+				.modelContainer(modelContainer)
+				.environment(
+					SubscriptionsViewModel(
+						modelContext: modelContainer.mainContext
+					)
+				)
 		}
-    }
+	}
 }
 
 #Preview {
-    RootView()
+	RootView()
 }
